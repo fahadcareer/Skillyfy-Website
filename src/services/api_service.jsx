278573f335de52866c70a11d17b37863bd5e9540
fahadcareer <mohamed.fahad@meerana.ae>
2025-11-14 10:56:39 +0530

    bug fix

--- src/services/api_service.jsx
+++ src/services/api_service.jsx
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://74.162.36.205:5075/skillyfy";
+const API_BASE_URL = "https://meerana-ai.uaenorth.cloudapp.azure.com/skillyfy";
 
 export async function apiGet(endpoint, headers = {}) {
     const res = await fetch(`${API_BASE_URL}${endpoint}`, { headers });
